ebfe8cfa317dacf5de22e89503718f27596d77c1
liammuro <liammuro123@icloud.com>
2023-10-24 15:10:56 -0400

    first commit

---
+++ LeibnitzForm.java
@@ -0,0 +1,38 @@
+public class LeibnitzForm {
+    public static float nextThingRec(float currentApprox, int lastOdd, boolean sign, int itersRem){
+        if (itersRem == 0) {return currentApprox; }
+
+        System.out.println(currentApprox);
+
+        int tempSign = -1;
+        lastOdd += 2;
+
+        if (sign) { tempSign = 1; }
+
+        currentApprox = currentApprox + (tempSign * (float)(4.0/lastOdd));
+    
+        return nextThingRec(currentApprox, lastOdd, !sign, itersRem-1);
+    }
+    public static float nextThingItr(int numIrts){
+
+        float currentApprox = (float)4.0;
+        int lastOdd = 1;
+        int sign = 1;
+
+        for (int i = 0; i < numIrts; i++) {
+            sign *= -1;
+            lastOdd += 2;
+
+            currentApprox = currentApprox + (sign * (float)(4.0/lastOdd));
+        }
+
+        
+        return currentApprox;
+    }
+    public static void main(String[] args) {
+        // System.out.println(nextThingRec((4/1), 1, false, 10001));
+
+        System.out.println(nextThingItr(10000000));
+
+    }
+}
